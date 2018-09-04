00ef2450d56be527ba2c5e8fb0391d72182bc6c3
Echevarria Arroyo <nayeli.echevarriaarroyo@stu.jefferson.kyschools.us>
2018-09-04 07:51:44 -0400

    first commit

---
+++ nayeli.py
@@ -0,0 +1,10 @@
+var = 0
+while var == 0 : # This constructs an infinite loop
+    num = raw_input("Enter a number  :8)
+                    print "you entered:  8
+                   ("enter a number  :17)
+                    print "you entered:  17
+                    ("enter a number :4)
+                     
+                     
+                    
